888baf0bf957776e709d80a2b873673aa821bd1d
martinachov <martin1082v@gmail.com>
2023-05-01 13:07:57 -0300

    first commit

---
+++ src/test/java/com/martinachov/github/actions/GithubActionsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martinachov.github.actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
